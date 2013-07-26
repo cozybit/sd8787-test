8b4c75664f3c1ff39cd06cf9fe67247cfdd2aaf5
Thomas Pedersen <thomas@cozybit.com>
2013-07-26 12:15:14 -0700

    improve common.sh
    add various helper functions, and try to fail early if
    possible.
    
    common.sh will now try to source a file named 'config' on
    invocation.

--- common.sh
+++ common.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+source config
+
 fail() {
 	echo "$@"
 	exit 1
 }
 
+testmode() {
+	local iface=$1
+	local testcase=$2
+	shift 2
+	local args=$@
+	sudo ./testmode.py -i$iface -t $testcase -a $args || fail
+}
+
 reload_card() {
 	sudo modprobe -r $DRIVER
+	# XXX: rfkill?
 	echo "reload card..."
 	read
 	sudo modprobe mwl8787_sdio
@@ -16,13 +27,44 @@ reload_card() {
 
 set_monitor() {
 	local iface=$1
-	sudo iw $iface set type monitor
+	link_down $iface
+	sudo iw $iface set type monitor || fail
+	link_up $iface
+}
+
+start_capture() {
+	local iface=$1
+	local file=$2
+	sudo tcpdump -i$iface -w$file
+}
+
+set_channel() {
+	local iface=$1
+	local ch=$2
+	sudo iw $iface set channel $ch || fail
 }
 
 link_up() {
 	local iface=$1
-	sudo ip link set $iface up
+	sudo ip link set $iface up || fail
+}
+
+link_down() {
+	local iface=$1
+	sudo ip link set $iface down || fail
+}
+
+fw_set_ch() {
+	local iface=$1
+	local ch=$2
+	testmode $iface set_channel $ch
+}
+
+fw_tx_data() {
+	local iface=$1
+	local data=$2
+	testmode $iface send_data $data
 }
 
 [ -z "$DRIVER" -o -z "$IFACE" ] && fail "please specify driver and iface"
-
+[ ! -z "$MON_IFACE" ] && set_monitor $MON_IFACE

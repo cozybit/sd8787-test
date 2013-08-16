9832f93e6e0360a900bb379c9e4c4ee7cf18e2c3
Javier Cardona <javier@cozybit.com>
2013-08-15 18:08:45 -0700

    Add two more tests to runall and fix PASS/FAIL result

--- runall.sh
+++ runall.sh
@@ -2,13 +2,13 @@
 
 mkdir -p logs
 
-tests="2-1-test_drv_load test_mon test_set_channel test_tx_unicast test_tx_mcast test_tx_bcn"
+tests="2-1-test_drv_load test_mon test_set_channel test_tx_unicast test_tx_mcast test_tx_bcn test_many_peers test_tx_feedback"
 
 for test in $tests; do
 
 	[ "${test}" = "test_mon" ] && ARG="noreload"
 
-	./$test ${ARG} | tee logs/${test}_log
+	./$test ${ARG}
 
 	if [ $? == 0 ]; then
 		printf "%-40s\e[00;32mPASS\e[00m\n" $test

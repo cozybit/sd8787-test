9f44108515f3349d2b2f7638767aab029d658090
Colleen Twitty <colleen@cozybit.com>
2014-01-02 10:31:33 -0800

    multi_vif: incorporate helper function from common.sh

--- test_multi_vif.sh
+++ test_multi_vif.sh
@@ -7,27 +7,34 @@ CAP_FILE=out.cap
 PAYLOAD="01234567890123456789"
 
 [ "$1" != "noreload" ] && reload_card
-PHY_8787="phy$(get_phy $IFACE)"
+PHY_8787=`if2phy $IFACE`
 
 # should use common's if_add here instead to add meshy if
 # then change type to mp
+echo "add mesh if..."
 
-iw phy $PHY_8787 interface add mesh0 type mp
+sudo iw phy $PHY_8787 interface add mesh0 type mp
 # change the mac address
-ifconfig mesh0 hw ether 00:19:88:52:75:86
-#ip addr add dev mesh0 192.168.0.55/24
-
+setmac mesh0 00:19:88:52:75:86
+sudo ip addr add dev mesh0 192.168.0.55/24
 
 # managed
-ip addr add dev $IFACE 192.168.0.33/24
+echo "add managed if..."
+sudo ip addr add dev $IFACE 192.168.0.33/24
 link_up $IFACE
-iw dev $IFACE connect -w cozyguest
+sudo iw dev $IFACE connect -w cozyguest || echo "failed to connect to ap"
 
-sleep 10
+declare -A bss1
+bss1[ssid]=mymesh
+bss1[channel]=11
+bss1[ch_type]=NO_HT
 
-iw dev mesh0 set channel 11
-link_up mesh0
+declare -A meshable
+meshable[if]="mesh0"
+set_bss meshable bss1
 
-sudo iw dev mesh0 mesh join mymesh
-sudo ifconfig mesh0 10.10.10.200
+# if_up in start_mesh is trying to add the routing magic
+start_mesh meshable
+
+#sudo ifconfig mesh0 10.10.10.200
 

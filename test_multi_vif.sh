04bb69a83cabe9c069f91753c02b32b8da6ce26b
Jacob Minshall <jacob@cozybit.com>
2014-01-02 13:27:07 -0800

    use functions to test individually

--- test_multi_vif.sh
+++ test_multi_vif.sh
@@ -8,33 +8,51 @@ PAYLOAD="01234567890123456789"
 [ "$1" != "noreload" ] && reload_card
 PHY_8787=`if2phy $IFACE`
 
-# should use common's if_add here instead to add meshy if
-# then change type to mp
-echo "add mesh if..."
+connect_mesh () {
+	# should use common's if_add here instead to add meshy if
+	# then change type to mp
+	echo "add mesh if..."
 
-sudo iw phy $PHY_8787 interface add mesh0 type mp
-# change the mac address
-setmac mesh0 00:19:88:52:75:86
+	sudo iw phy $PHY_8787 interface add mesh0 type mp
+	# change the mac address
+	setmac mesh0 00:19:88:52:75:86
 
-# managed
-echo "add managed if..."
-sudo ip addr add dev $IFACE 192.168.0.33/24
-link_up $IFACE
-sudo iw dev $IFACE connect -w cozyguest || echo "failed to connect to ap"
 
-declare -A bss1
-bss1[ssid]=mymesh
-bss1[channel]=11
-bss1[ch_type]=NO_HT
-bss1[ip]=192.168.0.55
+	declare -A bss1
+	bss1[ssid]=mymesh
+	bss1[channel]=11
+	bss1[ch_type]=NO_HT
+	bss1[ip]=192.168.0.55
 
-declare -A meshable
-meshable[if]="mesh0"
-set_bss meshable bss1
-assign_ip meshable bss1
+	declare -A meshable
+	meshable[if]="mesh0"
+	set_bss meshable bss1
+	assign_ip meshable bss1
 
-# if_up in start_mesh is trying to add the routing magic
-start_mesh meshable
+	# if_up in start_mesh is trying to add the routing magic
+	start_mesh meshable
+}
 
-#sudo ifconfig mesh0 10.10.10.200
+connect_ap () {
+	# managed
+	echo "add managed if..."
+	sudo ip addr add dev $IFACE 192.168.0.33/24
+	link_up $IFACE
+	sudo iw dev $IFACE connect -w cozyguest || echo "failed to connect to ap"
+}
 
+usage () {
+	echo "Usage: $0 [-a] [-m]\n-a for AP connection\n-m for mesh connection" 1>&2
+	exit
+}
+
+while getopts "ma" o; do
+	case "${o}" in
+		m)
+			connect_mesh;;
+		a)
+			connect_ap;;
+		*)
+			usage;;
+	esac
+done
